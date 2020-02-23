4e77e48cb391bda320d4a486a32182118ee10e96
dfloresdev <hello@dflores.dev>
2020-02-23 11:27:01 -0600

    Generated routes

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+  {
+    path: '/',
+    template: '<h1>Home</h1>'
+  },
+  {
+    path: '/contacto',
+    template: '<h1>Contact</h1>'
+  },
+  {
+    path: '/aboutme',
+    template: '<h1>About me</h1>'
+  }
+];
